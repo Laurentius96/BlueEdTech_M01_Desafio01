9956458517598f244957110aed4e1f0721427b3f
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-06-30 19:46:32 -0300

    Add início da história, pacote do prettier

---
+++ index.js
@@ -0,0 +1,7 @@
+/* História:
+
+ O ano é 1835, você se chama Friedrich Darwin parente de segundo grau do grande naturalista Charles Darwin. Ambos estão abordo do navio Beagle que é liderado pelo grande capitão FitzRoy. Vocês estão próximos do soltício de verão e estão atracados na ilha de Galápagos por volta de de 
+
+
+
+*/
