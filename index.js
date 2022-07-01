648421cedb20350379f357cf8084c5339d0bac34
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-06-30 21:17:15 -0300

    Continuação da história

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /* História:
 
- O ano é 1835, você se chama Friedrich Darwin parente de segundo grau do grande naturalista Charles Darwin. Ambos estão abordo do navio Beagle que é liderado pelo grande capitão FitzRoy. Vocês estão próximos do soltício de verão e estão atracados na ilha de Galápagos por volta de de 
-
+ A data é 16 de dezembro de 1835, você é o futuro maior naturalista da terra, Charles Darwin e está abordo do navio Beagle que é liderado pelo grande capitão FitzRoy. Há pouco vocês chegaram nas ilhas de Galápagos depois de uma incrível jornada que perdurou por meses ao mar. Entretanto, você que está sorrindo e animdo assim como toda a tripulação por finalmente ver terra firme, vai aos poucos trocando seu semblate por um de instigação ao perceber que os ventos que tocam seus ouvidos parecem carregar murmurios dizendo para irém embora...
+ 
+ A data é 17 de dezembro de 1835, você inicia sua ins
 
 
 */
