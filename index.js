4c94396c3bb80cc49e1737eeed7bb8676bc55415
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-07-03 12:02:49 -0300

    Correção de português

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 /* História:
 
- A data é 16 de dezembro de 1835, você é o futuro maior naturalista da terra, Charles Darwin e está abordo do navio Beagle que é liderado pelo grande capitão FitzRoy. Há pouco vocês chegaram nas ilhas de Galápagos depois de uma incrível jornada que perdurou por meses ao mar. Entretanto, você que está sorrindo e animdo assim como toda a tripulação por finalmente ver terra firme, vai aos poucos trocando seu semblate por um de instigação ao perceber que os ventos que tocam seus ouvidos parecem carregar murmurios dizendo para irém embora...
+ A data é 16 de dezembro de 1835 - manhã, você é o futuro maior naturalista da terra, Charles Darwin e está abordo do navio Beagle que é liderado pelo grande capitão FitzRoy. Há pouco vocês chegaram nas ilhas de Galápagos depois de uma incrível jornada que perdurou por meses ao mar. Entretanto, você que está sorrindo e animado assim como toda a tripulação por finalmente ver terra firme, vai aos poucos trocando seu semblante por um de instigação ao perceber que os ventos que tocam seus ouvidos parecem carregar murmúrios quase inaudíveis dizendo: "Sacrifícios!!"
  
- A data é 17 de dezembro de 1835, você inicia sua ins
+  A data é 17 de dezembro de 1835, na manhã seguinte você junto com mais 5 marinheiros vão desembarcar e fazer uma expedição as ilhas de Galápagos para estudo da flora e fauna local. 
 
 
 */
+const prompt = require('prompt-sync')();
+
+let dificuldade;
+
